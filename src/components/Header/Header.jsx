41f0c6fe486bfb9e8ec3143f7418378512372894
AndreyF <getsuga@tut.by>
2021-10-05 15:53:08 +0300

    Initial project version

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Header.scss";
+import logo from "../../assets/pizza-logo.png";
+
+const Header = (props) => {
+  return (
+    <div className="header">
+      <div className="header__container">
+        <div className="header__inner">
+          <div className="header__logo">
+            <a href="/" className="header__logo--link">
+              <img src={logo} alt="логотип" />
+            </a>
+          </div>
+          <h1>PIZZA-STORE</h1>
+          <div className="header__cart">
+            <button className="header__btn" type="button">
+              Корзина
+            </button>
+          </div>
+        </div>
+        <nav className="header__nav">
+          <ul className="header__nav--list">
+            <li className="header__nav--list--item">
+              <a href="/">Пицца</a>
+            </li>
+            <li className="header__nav--list--item">
+              <a href="/">Закуски</a>
+            </li>
+            <li className="header__nav--list--item">
+              <a href="/">Напитки</a>
+            </li>
+            <li className="header__nav--list--item">
+              <a href="/">О нас</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
